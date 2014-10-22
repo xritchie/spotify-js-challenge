13d8bc0b3f4f6c6cf730a86a5c493bb29428701d
Shawn Ritchie <xritchie@gmail.com>
2014-10-22 20:39:24 +0200

    Challenge 1

--- custom-DOM-wrapper/spotify-dom.js
+++ custom-DOM-wrapper/spotify-dom.js
@@ -1,3 +1,5 @@
+'use strict'
+
 /**
  * Constructor for the SpotifyDOM object.
  *
@@ -7,7 +9,40 @@
  *  - tag name e.g. 'li'
  * @return {SpotifyDOM}
  */
-var SpotifyDOM = function(selector) {};
+var SpotifyDOM = function(selector) {
+
+	if (!(this instanceof SpotifyDOM))
+		return new SpotifyDOM(selector);
+
+	if (!selector)
+		this.docs = document;
+	else if (selector.docs)
+		this.docs = selector.docs;
+	else if (typeof selector == 'string' || selector instanceof String)
+	{
+		var updateSelector = function(selector) {
+			return (['.','#'].indexOf(selector[0]) != -1) ? selector.slice(1, selector.length) : selector;
+		};
+
+		var method = function(selector) {
+			switch(selector[0]) {
+			    case '.':
+			        return document.getElementsByClassName;
+			        break;
+			    case '#':
+			        return document.getElementById;
+			        break;
+			    default:
+			        return document.getElementsByTagName;
+			}
+		}
+
+		this.docs = method(selector).apply(document, [updateSelector(selector)]);
+	}
+
+
+	return this;
+};
 
 /**
  * Changes or returns the content of the element.
@@ -20,7 +55,21 @@ var SpotifyDOM = function(selector) {};
  * is a collection,
  *  e.g. SpotifyDOM(‘li’).html(‘test’)
  */
-SpotifyDOM.prototype.html = function(content) {};
+SpotifyDOM.prototype.html = function(content) {
+	var isCollection = this.isCollection();
+	if  (isCollection && (this.docs.length != 1))
+			throw "SpotifyDOMInvalidElement"; 
+
+	var updateDoc = function(doc) {
+		if (!content)
+			return doc.innerHTML;
+
+		doc.innerHTML = content;
+		return this;
+	}.bind(this);
+
+	return (!isCollection) ? updateDoc(this.docs) : updateDoc(this.docs[0]);
+};
 
 /**
  * Applies the callback to every element of the collection, the callback will
@@ -32,10 +81,30 @@ SpotifyDOM.prototype.html = function(content) {};
  * @param {Function} callback
  * @return {SpotifyDOM}
  */
-SpotifyDOM.prototype.each = function(callback) {};
+SpotifyDOM.prototype.each = function(callback) {
+	if (this.isCollection())
+		for (var i = 0; i < this.docs.length; i++) 
+			callback(new SpotifyDOM({docs: this.docs[i]}));
+
+	return this;
+};
 
 /**
  * @method isCollection
  * @return {Boolean} true if the currently selected element is a collection(e.g. ‘li’).
  */
-SpotifyDOM.prototype.isCollection = function() {};
+SpotifyDOM.prototype.isCollection = function() {
+	return this.docs && Object.prototype.toString.call(this.docs) === '[object HTMLCollection]';	
+};
+
+/**
+ * @test - run on load the check all the functionality is working correctly
+ */
+window.onload = function() {
+	console.log(SpotifyDOM());
+	console.log(SpotifyDOM('#p1').html());
+	console.log(SpotifyDOM('#p1').html('RESET'));
+	console.log(SpotifyDOM('.paragraph').html());
+	SpotifyDOM('li').each(function(content) {console.log(content.html())});
+}
+
