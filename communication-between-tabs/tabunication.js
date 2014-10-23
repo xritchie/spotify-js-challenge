f9c447c9986b74b60923993bdadd7377bf951498
Shawn Ritchie <xritchie@gmail.com>
2014-10-23 22:49:50 +0200

    Finshed multi tab communication

--- communication-between-tabs/tabunication.js
+++ communication-between-tabs/tabunication.js
@@ -1,94 +1,147 @@
 'use strict'
 
-window.cookieEngine = (function() {
-	function CookieEngine() {}
+window.CookieEngine = (function() {
+	function CookieEngine() {
+		if (!(this instanceof CookieEngine))
+			return new CookieEngine();
 
-	CookieEngine.prototype.push = (function(key, value, days) {
-		var expires = "";
-		if (days) {
-	        var date = new Date();
-	        date.setTime(date.getTime()+(days*24*60*60*1000));
-	        var expires = "; expires="+date.toGMTString();
-	    }
-	    document.cookie = name+"="+value+expires+"; path=/";
-	    return this;
-	});
+		return this;
+	}
 
-	CookieEngine.prototype.pop = (function(key) {
-		var value = this.get(key);
-		this.push(key,"",-1);
-	});
-	
-	CookieEngine.prototype.get = (function(key) {
-		var value = "; " + document.cookie;
-	  	var parts = value.split("; " + key + "=");
-		if (parts.length == 2) return parts.pop().split(";").shift();
-	});
+	CookieEngine.prototype = {
+		push: function(key, value, days) {
+			var expires = "";
+			if (days) {
+		        var date = new Date();
+		        date.setTime(date.getTime()+(days*24*60*60*1000));
+		        var expires = "; expires="+date.toGMTString();
+		    }
+		    document.cookie = name+"="+value+expires+"; path=/";
+		    return this;
+		},
+		pop: function(key) {
+			var value = this.get(key);
+			this.push(key,"",-1);
+			return value;
+		},
+		get: function(key) {
+			var value = "; " + document.cookie;
+		  	var parts = value.split("; " + key + "=");
+			if (parts.length == 2) return parts.pop().split(";").shift();
+			return null;
+		},
+		isEnabled: function() {
+			return navigator.cookieEnabled;
+		}
+	};
 
-	CookieEngine.prototype.isEnabled = (function() {
-		return navigator.cookieEnabled;
-	});
-});
+	return CookieEngine;
+})();
 
+window.LocalStorageEngine = (function() {
+	function LocalStorageEngine() {
+		if (!(this instanceof LocalStorageEngine))
+			return new LocalStorageEngine();
 
-window.localStorageEngine = (function() {
-	function LocalStorageEngine() {}
+		return this;
+	}
 
-	LocalStorageEngine.prototype.push = (function(key, value) {
-		localStorage.setItem(key, value);
-	});
+	LocalStorageEngine.prototype = {
+		push: function(key, value) {
+			localStorage.setItem(key, value);
+			return this;
+		},
+		pop: function(key) {
+			var value = this.get(key);
+			localStorage.removeItem("lastname");
+			return value;
+		},
+		get: function(key) {
+			return localStorage.getItem(key);
+		},
+		isEnabled: function() {
+			return (typeof(Storage) !== "undefined");
+		}
+	};
 
-	LocalStorageEngine.prototype.pop = (function(key) {
-		var value = this.get(key);
-		localStorage.removeItem("lastname");
-		return value;
-	});
+	return LocalStorageEngine;
+})();
 
-	LocalStorageEngine.prototype.get = (function(key) {
-		localStorage.getItem(key);
-	});
+window.CustomEvent = (function () {
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   };
 
-	LocalStorageEngine.prototype.isEnabled = (function() {
-		return (typeof(Storage) !== "undefined");
-	});
-});
+  CustomEvent.prototype = window.Event.prototype;
+
+  return CustomEvent;
+})();
 
 window.tabunication = (function() {
 
 	var instance;
 
+	var fireEvent = function(eventId, payload) {
+		var event = new CustomEvent(eventId, payload);
+		document.dispatchEvent(event);
+	};
+
 	function Tabunication(engine) {
-
-
-		if (!(this instanceof SpotifyDOM))
+		if (!(this instanceof Tabunication))
 			return new Tabunication(selector);
 
-		var engine = (!engine) ? window.localStorageEngine : engine;
+		this.engine = (!engine) ? new LocalStorageEngine() : engine;
+
+		document.addEventListener("tabunication:push", function(e) {
+			console.log("pushed: " + e.detail.key);
+		});
+
+		document.addEventListener("tabunication:pop", function(e) {
+			console.log("poped: " + e.detail.key);
+		});
+
+		window.addEventListener('storage', function (e){
+		    console.log("key: " + e.key);
+		    console.log("newValue: " + e.newValue);  
+		    console.log("oldValue: " + e.oldValue);  
+		    console.log("url: " + e.url);  
+		    console.log("storageArea: " + e.storageArea);  
+		});
 
 		return this;
-	}
+	};
 
-	Tabunication.prototype.push = (function (key, value) {
-		return this.engine.push(key, value);
-	});
+	Tabunication.prototype = {
+		push: function (key, value) {
+			this.engine.push(key, value);
+			fireEvent('tabunication:push', {'detail': {'key': key}});
+			return this;
+		},
+		pop: function (key) {
+			fireEvent('tabunication:pop', {'detail': {'key': key}});
+			return this.engine.pop(key);
+		},
+		get: function (key) {
+			return this.engine.get(key)
+		}
+	};
 
-	Tabunication.prototype.pop = (function (key) {
-		return this.engine.pop(key);
-	});
-
-	Tabunication.prototype.get = (function (key) {
-		return this.engine.get(key)
-	});
-
-	var tabunication = {
-		get: function() {
+	return {
+		getInstance: function() {
 			if (!instance) {
 				instance = new Tabunication();
 			}
 			return instance;
+		},
+		create: function() {
+			return this.get();
 		}
 	};
+})();
 
-	return tabunication;
-
-}());
+window.onload = function() {
+    window.tabunication.getInstance();
+}
